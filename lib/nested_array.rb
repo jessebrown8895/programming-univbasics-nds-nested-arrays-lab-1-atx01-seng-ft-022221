d2bff96ac823e31382aa6afb360347e443019b90
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-04 21:42:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,8 +26,7 @@ def sorted_matrix
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character 
   #ORGANIC_PRODUCE = [
-  
-  all_organic_produce = [["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"], ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"]]
+  ORGANIC_PRODUCE.sort
   
 end
 
