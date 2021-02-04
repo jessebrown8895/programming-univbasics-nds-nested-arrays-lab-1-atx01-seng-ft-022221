21a8515d3eca3d08df494818cffd1ac63c45d7f5
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-04 22:11:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-matrix_lookupORGANIC_PRODUCE([2][5])
+ORGANIC_PRODUCE([2][5])
   
 end
 
